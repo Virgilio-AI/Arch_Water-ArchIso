6d428d00a70d9f606a89fdd052ba3f9eb4df924f
Virgilio Murillo <virgiliomurilloochoa1@gmail.com>
2021-08-06 04:38:24 +0000

    added the installer script for the main installation

--- build.sh
+++ build.sh
@@ -2,60 +2,65 @@ shopt -s extglob
 ## clean folders and makepkg of github repos
 echo "do you want to update the data base?(y,n)"
 read temp
+
 if [ $temp == "y" ] || [ $temp == "" ];
 then
-	
-	cd PKGBUILDS/dwm/ &&
-	rm -vrfd !("PKGBUILD") &&
-	makepkg &&
-	
-	cd ../dwmblocks &&
-	rm -vrfd !("PKGBUILD") &&
-	makepkg &&
-	
-	cd ../cfiles &&
-	rm -vrfd !("PKGBUILD") &&
-	makepkg &&
-	
-	cd ../dmenu &&
-	rm -vrfd !("PKGBUILD") &&
-	makepkg &&
-	
-	cd ../st &&
-	rm -vrfd !("PKGBUILD") &&
-	makepkg &&
-	
 	# clean arch packages and makepkg
-	cd .. &&
+	cd PKGBUILDS &&
 	rm -rfdv brave-bin &&
 	paru -G brave-bin &&
 	cd brave-bin &&
 	makepkg &&
-	
+
 	cd .. &&
 	rm -rfdv libxft-bgra &&
 	paru -G libxft-bgra &&
 	cd libxft-bgra &&
 	makepkg &&
-	
-	
+
+
 	cd .. &&
 	rm -rfdv paru &&
 	paru -G paru &&
 	cd paru &&
 	makepkg &&
-	
-	
+
+
 	cd .. &&
 	rm -rfdv vim-plug &&
 	paru -G vim-plug &&
 	cd vim-plug &&
 	makepkg &&
-	
+
+	# custom repos
+	cd ../dwm &&
+	rm -vrfd !("PKGBUILD") &&
+	makepkg &&
+
+	cd ../dwmblocks &&
+	rm -vrfd !("PKGBUILD") &&
+	makepkg &&
+
+	cd ../cfiles &&
+	rm -vrfd !("PKGBUILD") &&
+	makepkg &&
+
+	cd ../dmenu &&
+	rm -vrfd !("PKGBUILD") &&
+	makepkg &&
+
+	cd ../st &&
+	rm -vrfd !("PKGBUILD") &&
+	makepkg &&
+
+	cd ../autoInstaller-AW &&
+	rm -vrfd !("PKGBUILD") &&
+	makepkg &&
+
 	# repo-add all the packages
 	cd .. &&
 	rm -rfd CustomRepo/* &&
-	
+
 	# repo-add CustomRepo/CustomRepo.db.tar.gz brvae-bin/brave
 	find brave-bin -name brave-bin*.pkg.tar.zst -exec repo-add CustomRepo/CustomRepo.db.tar.gz {} \; &&
 	find cfiles -name cfiles*.pkg.tar.zst -exec repo-add CustomRepo/CustomRepo.db.tar.gz {} \; &&
@@ -66,6 +71,7 @@ then
 	find st -name st*.pkg.tar.zst -exec repo-add CustomRepo/CustomRepo.db.tar.gz {} \; &&
 	find vim-plug -name vim-plug*.pkg.tar.zst -exec repo-add CustomRepo/CustomRepo.db.tar.gz {} \; &&
 	find libxft-bgra -name libxft-bgra*.pkg.tar.zst -exec repo-add CustomRepo/CustomRepo.db.tar.gz {} \; &&
+	find autoInstaller-AW -name autoInstaller-AW*.pkg.tar.zst -exec repo-add CustomRepo/CustomRepo.db.tar.gz {} \; &&
 	
 	
 	# paste the pkg inside of the repository
@@ -78,7 +84,8 @@ then
 	cp -frv st/* CustomRepo/ &&
 	cp -frv vim-plug/* CustomRepo/ &&
 	cp -frv libxft-bgra/* CustomRepo/ &&
-	cd ..
+	cp -frv autoInstaller/* CustomRepo/ &&
+	cd ../..
 fi
 
 sudo rm -rfdv out ; sudo rm -rfdv work &&
